aa04a9602fb7de31e3c7817bc2885224e2b6120c
chinatsu <lolexplode@gmail.com>
2016-08-26 20:22:16 +0200

    Initial commit
    Sorry in advance for writing code without thinking

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import scrape
+import arrow
+
+app = Flask(__name__)
+app.config["response"] = scrape.get_timetable(['IS-100', 'IS-104', 'IS-109', 'IS-111']) # initialize response
+app.config['timestamp'] = arrow.now() # set a timestamp, i like arrow because it's easy
+
+@app.route('/timeplan')
+def load_schedule():
+    if app.config['timestamp'] < arrow.now().replace(hours=-6):
+        app.config["response"] = scrape.get_timetable(['IS-100', 'IS-104', 'IS-109', 'IS-111'])
+        app.config['timestamp'] = arrow.now()
+    return render_template('timetable.html', weeks=app.config['response']) # render the response!
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=3000, debug=True)
---
+++ scrape/__init__.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_course(course):
+    return BeautifulSoup(requests.get('http://timeplan.uia.no/swsuiah/XMLEngine/default.aspx?ModuleByWeek&' +
+                                      'p1=;{}-1;&p2=32;33;34;35;36;37;38;39;'.format(course) +
+                                      '40;41;42;43;44;45;46;47;48;49;50;51').text, 'lxml')
+
+def get_timetable(courses):
+    soups = []
+    for course in courses:
+        soups.append(get_course(course)) # populate our list with each course's timetable
+
+    weeks = {}
+    for soup in soups[0].find_all('table'):
+        week = int(soup.find('td').text.split(' ')[1][:-1]) # get the current week number
+        weeks[week] = [] # initialize each week in our weeks dictionary
+
+    for soup in soups:
+        for x in soup.find_all('table'): # each table is a week
+            week = int(x.find('td').text.split(' ')[1][:-1]) # return of the hacky integer
+            tr2 = x.find_all(class_='tr2') # tr2 is the class that has all the "real" data
+            for entry in tr2:
+                data = {}
+                z = entry.find_all('td') # each td has a set format with important data
+                data['datetime'] = "{} {}".format(z[1].text, z[2].text) # used for sorting later
+                data['weekday'] = z[0].text
+                data['date'] = z[1].text
+                data['time'] = z[2].text
+                data['details'] = z[3].text
+                data['location'] = z[4].text
+                data['lecturer'] = z[5].text
+                weeks[week].append(data) # append our data to the appropriate week
+    for x in weeks:
+        weeks[x] = sorted(weeks[x], key=lambda y: y['datetime']) # i love lexical sorting
+    return weeks
